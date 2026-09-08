fd9161189df5880e736d826cc9d88b2701251a9f
anthonybcit <acao11@my.bcit.ca>
2026-09-07 21:49:33 -0700

    html?

--- routes.py
+++ routes.py
@@ -3,6 +3,11 @@ import uuid
 from flask import Flask, render_template, request, redirect, url_for
 from config import Config
 from pymongo import MongoClient
+import logging
+from botocore.exceptions import ClientError
+from botocore.config import Config as BotoConfig
+
+
 
 
 mongo = MongoClient(Config.MONGO_URI)
@@ -21,7 +26,7 @@ def detect_labels(photo, bucket):
             MaxLabels=10, MinConfidence=60)
     labellist = []
     for label in response["Labels"]:
-        labellist.append({"name": label["Name"], "confidence": label["Confidence"]})
+        labellist.append({"name": label["Name"].lower(), "confidence": label["Confidence"]})
     return labellist
 
 maindb = mongo 
@@ -54,7 +59,68 @@ def index():
         return redirect(url_for("index"))
         
     return render_template("index.html")
+@app.route("/search")
+def search():
+    q = request.args.get("q", "").lower()
+
+    print("SEARCH:", q)
+
+    if q:
+        results = list(labeldb.find({"labels.name": q}))
+    else:
+        results = []
+
+    print("RESULTS:", results)
+
+    s3 = boto3.client(
+        "s3",
+        region_name=region,
+        config=BotoConfig(
+            signature_version="s3v4",
+            s3={"addressing_style": "virtual"}
+        ),
+    )
+
+    for photo in results:
+        photo["url"] = s3.generate_presigned_url(
+            "get_object",
+            Params={
+                "Bucket": Config.bucket_name,
+                "Key": photo["name"]
+            },
+            ExpiresIn=3600
+        )
+
+    return render_template("results.html", results=results)
 
 @app.route("/photos", methods=["GET", "POST"])
 def photos():
-    return render_template("photos.html")
+    photos =  list(labeldb.find())
+    s3 = boto3.client(
+        "s3",
+        region_name=region,
+        config=BotoConfig(signature_version="s3v4", s3={"addressing_style": "virtual"}),
+    )
+    for photo in photos:
+        photo["url"] = s3.generate_presigned_url(
+            "get_object",
+            Params={
+                "Bucket": Config.bucket_name,
+                "Key": photo["name"]
+            },
+            ExpiresIn=3600
+        )
+
+    return render_template("photos.html", photos=photos)
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    name = request.form.get("name")
+    s3 = boto3.client("s3", region_name=region)
+    s3.delete_object(
+        Bucket=Config.bucket_name,
+        Key=name
+    )
+    labeldb.delete_one({"name": name})
+
+    return redirect(url_for("photos"))
